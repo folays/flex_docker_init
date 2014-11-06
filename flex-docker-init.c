cceb32380f969db4eee9f0ac4c4fa9b960393a1c
root <root@myvm.online.net>
2014-11-06 17:59:08 +0100

    .

--- flex-docker-init.c
+++ flex-docker-init.c
@@ -4,10 +4,39 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
+
+#define FLEX_TERM_WAIT_TIME 5
+
+static void _wait_reap_all(void)
+{
+  int start_time;
+
+  start_time = time(NULL);
+  while (kill(-1, 0) != -1 && time(NULL) - FLEX_TERM_WAIT_TIME < start_time)
+    {
+      int status;
+
+      // reap all process, probably killed by SIGTERM above.
+      while (wait(&status) != -1)
+	;
+
+      usleep(500000);
+    }
+}
 
 static void signal_term(int sig)
 {
   kill(-1, SIGTERM);
+  _wait_reap_all();
+
+  kill(-1, SIGKILL);
+  _wait_reap_all();
+
+  signal(SIGTERM, SIG_DFL);
+  kill(getpid(), SIGTERM);
+
+  exit(1);
 }
 
 static void signal_chld(int sig)
